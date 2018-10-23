dff740a3e91ac3fef853c74d4521c21c1ab39270
Antonio <antonio@antonio101.com>
2018-10-22 20:18:01 -0700

    Tutorial commit

--- LivingPortraitFaceDetection.py
+++ LivingPortraitFaceDetection.py
@@ -1,5 +1,5 @@
 '''
-Haar Cascade Face detection with OpenCV  
+Moving portrait with haar cascade face detection with OpenCV
 '''
 from time import sleep
 from omxplayer.player import OMXPlayer
@@ -10,37 +10,13 @@ import sys
 import cv2
 
 # multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
-CASCADE_PATH = Path('/home/antonio/haarcascade_frontalface_default.xml')
-VIDEO_PATH = Path('/home/antonio/raspberrypi_halloween_video.mp4')
+CASCADE_PATH = Path('haarcascade_frontalface_default.xml')
+VIDEO_PATH = Path('raspberrypi_halloween_video.mp4')
+# Adjust the dimensions according to you monitor
 VIDEO_DIMENSIONS = '0 0 {LENGTH} {WIDTH}' \
                    .format(LENGTH='1920', WIDTH='1080')
 OMX_PLAYER_ARGS = ['--no-osd', '--loop', '--win', VIDEO_DIMENSIONS]
 KILL_OMXPLAYER_PROCESS = 'sudo pkill omxplayer.bin'
-
-#Scary Portrait Times:
-#Man:
-#Total Time: 0:00 -> 1:09
-#Initial scene: 0:00
-#Scene 1: 0:03 -> 0:13
-#Scene 2: 0:14 -> 0:36
-#Scene 3: 0:37-> 0:56
-#Scene 4: 0:57 -> 1:09
-
-#Woman:
-#Total Time: 1:11 -> 2:09
-#Initial scene: 1:11
-#Scene 1: 1:11 -> 1:28
-#Scene 2: 1:28 -> 1:41
-#Scene 3: 1:41 -> 1:55
-#Scene 4: 1:56 -> 2:08
-
-#Girl:
-#Total Time: 2:10 ->
-#Initial scene: 2:10
-#Scene 1: 2:10 -> 2:37
-#Scene 2: 2:37 -> 2:51
-#Scene 3: 2:51 -> 3:01
-#Scene 4: 3:01 -> 3:12
 
 START_OF_SCENE = [0, 71, 130]
 
@@ -64,6 +40,7 @@ GIRL_SCENE_TIMES = {
   2: [171, 181],
   3: [181, 192],
 }
+
 LIST_OF_SCENES = [MAN_SCENE_TIMES, WOMAN_SCENE_TIMES, GIRL_SCENE_TIMES]
 
 def cmdline(command):
@@ -73,12 +50,12 @@ def cmdline(command):
   return process.communicate()[0]
 
 def main():
-  try:
     """
-      Insert 'python3 /home/antonio/faceDetection.py &'
+      Insert 'python3 <YOUR_PATH>/LivingPortraitFaceDetection.py &'
       to /etc/rc.local in order for the script to run
       during boot.
     """
+  try:
     sleep(30) # Allow plenty of time for everything to load
     if not CASCADE_PATH.exists() or not VIDEO_PATH.exists():
       print("{0}|{1} File(s) not found!" \
@@ -120,38 +97,26 @@ def main():
                                                minNeighbors=5,
                                                minSize=(30, 30))
           if len(faces) > 0:
-            #print("Face detected in image")
             if not is_face_on_first_pass:
-              #print("Face detected on the first pass!")
               is_face_on_first_pass = True
               face_detected = True
               continue
             if not is_face_on_second_pass:
-              #print("Face detected on the second pass!")
               is_face_on_second_pass = True
             if is_face_on_first_pass and is_face_on_second_pass:
-              #print("Face detected on both passes!")
               is_face_on_first_pass = False
               is_face_on_second_pass = False
               face_detected = True
-            #else:
-            #  print("Keep on trying!")
-            #  is_face_on_first_pass = False
-            #  is_face_on_second_pass = False
-            #  continue
-          #else:
           if is_face_on_first_pass or is_face_on_second_pass:
             is_face_on_first_pass = False
             is_face_on_second_pass = False
             face_detected = False
-            #print("No faces detected, clearing flags")
+            # No faces detected, clearing flags
             continue
 
           if face_detected:
-            #print("Face detected. Start video")
+            # Face detected. Start video
             face_detected = True
-            #print('{0} Face(s) Detected!'.format(len(faces)))
-            #print('Playing scene : {0} => {1}'.format(scene_count, face_detected_count))
             selected_scene = LIST_OF_SCENES[scene_count]
             scary_scene = selected_scene[face_detected_count]
             player.set_position(scary_scene[0])
--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # ScaryLivingPortrait
 RaspberryPi Halloween Project
+
+## Parts needed:
+- Raspberry Pi 2|3
+- Raspberry Pi Camera module v2|v3
+- Unliving portrait video(s): [AtmostFX] (https://atmosfx.com/collections/halloween/products/unliving-portraits)
+- Spare TV or monitor
+- Picture Frame (I created mine using wall molding)
+- Speaker(s)
+- Good ventilation on the CPUs of the Raspberry Pi
+
+## Prereqs
+I'm not going go over how to configure all the prereqs. There is
+plenty of documentation out there to show you the way.
+- Raspberry Pi version 2 or 3 running the latest Raspbian.
+- OpenCV 3.4.3 (Any version should be ok).
+- Python3.
+- Omxplayer is pre-installed on Raspbian 9.4. (Stretch).
+- Omxplayer-wrapper python library.
+```bash
+  sudo python3 -m pip install omxplayer-wrapper
+```
+<!-- TODO Insert Link for tutorial -->
+
+You should now have everything you need to run the script.
+
+### Scary Portrait Video:
+Stitched up 3 different videos so that you can alternate between different
+characters.
+Video file: raspberrypi_halloween_video.mp4 (not included)
+Total length = 3:25 min
+
+Man:
+Total Time: 0:00 -> 1:09
+Initial scene: 0:00
+Scene 1: 0:03 -> 0:13
+Scene 2: 0:14 -> 0:36
+Scene 3: 0:37 -> 0:56
+Scene 4: 0:57 -> 1:09
+
+Woman:
+Total Time: 1:11 -> 2:09
+Initial scene: 1:11
+Scene 1: 1:11 -> 1:28
+Scene 2: 1:28 -> 1:41
+Scene 3: 1:41 -> 1:55
+Scene 4: 1:56 -> 2:08
+
+Girl:
+Total Time: 2:10 -> 3:15
+Initial scene: 2:10
+Scene 1: 2:10 -> 2:37
+Scene 2: 2:37 -> 2:51
+Scene 3: 2:51 -> 3:01
+Scene 4: 3:01 -> 3:12
+
+The Result:
+Essentially, it will iterate through all 3 videos in which
+each video clip has 4 different scenes.
+Take a look at the finish product.
+[![Imgur](https://i.imgur.com/enEWNMx.png)](https://youtu.be/Ghp6V88iJZM)
